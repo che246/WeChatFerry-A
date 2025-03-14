f73ec02c8bb8371806923d99fc7da6e108299174
fucky <1247288742@qq.com>
2025-03-15 07:53:47 +0800

    更新 dllmain.cpp
    增加适配WINDOWS10 64位以上版本隐藏 DLL功能

--- WeChatFerry/spy/dllmain.cpp
+++ WeChatFerry/spy/dllmain.cpp
@@ -1,10 +1,37 @@
-﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "framework.h"
+#include <Windows.h>
+#include <TlHelp32.h>
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
+BOOL HideDll(HMODULE hModule) {
+    MODULEENTRY32 moduleEntry;
+    moduleEntry.dwSize = sizeof(MODULEENTRY32);
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId());
+
+    if (snapshot == INVALID_HANDLE_VALUE) {
+        return FALSE;
+    }
+
+    BOOL moduleFound = Module32First(snapshot, &moduleEntry);
+
+    while (moduleFound) {
+        if (moduleEntry.hModule == hModule) {
+            memset(moduleEntry.szModule, 0, sizeof(moduleEntry.szModule));
+            memset(moduleEntry.szExePath, 0, sizeof(moduleEntry.szExePath));
+            break;
+        }
+        moduleFound = Module32Next(snapshot, &moduleEntry);
+    }
+
+    CloseHandle(snapshot);
+    return TRUE;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
+            HideDll(hModule);
+            break;
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
